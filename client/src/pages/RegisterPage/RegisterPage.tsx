d80e04d152d2c4f8084eba2a02392ac9c6550ad2
Vladimir <grosswladimir@yandex.ru>
2023-10-19 18:10:40 +0300

    added input handling in RegisterPage component

--- client/src/pages/RegisterPage/RegisterPage.tsx
+++ client/src/pages/RegisterPage/RegisterPage.tsx
@@ -1,25 +1,93 @@
+import React, { useState } from 'react';
 import './RegisterPage.scss';
 
-const RegisterPage = () => {
+const RegisterPage: React.FC = () => {
+    const [nameValue, setNameValue] = useState('');
+    const [lastNameValue, setLastNameValue] = useState('');
+    const [emailValue, setEmailValue] = useState('');
+    const [passwordValue, setPasswordValue] = useState('');
+
+    const handleRegisterUser = () => {
+        console.log(nameValue)
+        console.log(lastNameValue)
+        console.log(emailValue)
+        console.log(passwordValue)
+        setNameValue('');
+        setLastNameValue('');
+        setEmailValue('');
+        setPasswordValue('');
+    }
+
+    const hendleNameInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setNameValue(e.target.value)
+    }
+    
+    const hendleLastNameInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setLastNameValue(e.target.value)
+    }
+
+    const hendleEmailInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setEmailValue(e.target.value)
+    }
+
+    const hendlePasswordInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setPasswordValue(e.target.value)
+    }
+    
+
     return (
         <div className="card">
             <div className="mb-3">
                 <label htmlFor="register-name-input" className="form-label">Name</label>
-                <input type="name" className="form-control" id="register-name-input" placeholder="Введите имя" />
+                <input 
+                    type="name" 
+                    className="form-control" 
+                    id="register-name-input" 
+                    placeholder="Введите имя" 
+                    value={nameValue} 
+                    onChange={hendleNameInputChange}
+                />
             </div>  
             <div className="mb-3">
                 <label htmlFor="register-lastName-input" className="form-label">Last Name</label>
-                <input type="lastName" className="form-control" id="register-lastName-input" placeholder="Введите фамилию" />
+                <input 
+                    type="lastName" 
+                    className="form-control" 
+                    id="register-lastName-input" 
+                    placeholder="Введите фамилию" 
+                    value={lastNameValue} 
+                    onChange={hendleLastNameInputChange}
+                />
             </div> 
             <div className="mb-3">
                 <label htmlFor="register-email-input" className="form-label">Email address</label>
-                <input type="email" className="form-control" id="register-email-input" placeholder="Введите почту" />
+                <input 
+                    type="email" 
+                    className="form-control" 
+                    id="register-email-input" 
+                    placeholder="Введите почту" 
+                    value={emailValue} 
+                    onChange={hendleEmailInputChange}
+                />
             </div> 
             <div className="mb-3">
                 <label htmlFor="register-password-input" className="form-label">Password</label>
-                <input type="password" className="form-control" id="register-password-input" placeholder="Введите пароль" />
+                <input 
+                    type="password" 
+                    className="form-control" 
+                    id="register-password-input" 
+                    placeholder="Введите пароль" 
+                    value={passwordValue} 
+                    onChange={hendlePasswordInputChange}
+                />
             </div>  
-            <button type="button" className="btn btn-primary">Register</button>
+            <button 
+                type="button" 
+                className="btn btn-primary"
+                onClick={handleRegisterUser}
+            >
+                Register
+            </button>
         </div>
     )
 }
